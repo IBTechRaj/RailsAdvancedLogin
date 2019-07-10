c4c1105337dcbba6493234fc7048763678f29890
ibtechraj <raj_shk@rediffmail.com>
2019-07-10 20:55:04 +0530

    Finished Advanced Login Ch 9

--- sample_app/test/models/user_test.rb
+++ sample_app/test/models/user_test.rb
@@ -8,4 +8,7 @@ def setup
   # test "the truth" do
   #   assert true
   # end
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
 end
